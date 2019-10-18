880f31168dcbea3025d9f35fe1b38fd398a88e3a
cnease10 <cierra.nease10@gmail.com>
2019-10-18 14:13:55 -0600

    have controller set up with index route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
 
+//middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+
+//setup controller
+const movieController = require('./controllers/movies');
+app.use('/movies', movieController);
+
+
+//server is up
 app.listen(3000, () => {
     console.log('we in this');
 });
